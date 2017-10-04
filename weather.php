b5a17e39ac501384653ea3b2c14cac17f3102886
Fastrok <32041982+Fastrok@users.noreply.github.com>
2017-10-04 22:17:04 +0300

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,1 +1,7 @@
-
+<?php
+$city_id=524901;
+$weather="http://api.openweathermap.org/data/2.5/weather?id=$city_id&mode=xml&lang=ru&units=metric&APPID=5888b8b93ed8d1906c9665e31bed55ff";
+$test = file_get_contents($weather,0);
+$xm = new SimplexmlElement($test);
+print_r($xm);
+?>
