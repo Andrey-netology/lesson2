9741c932bfa64dd4dd074da81737335182957ed8
Fastrok <32041982+Fastrok@users.noreply.github.com>
2017-10-04 23:14:01 +0300

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,7 +1,22 @@
 <?php
-$city_id=524901;
-$weather="http://api.openweathermap.org/data/2.5/weather?id=$city_id&mode=xml&lang=ru&units=metric&APPID=5888b8b93ed8d1906c9665e31bed55ff";
-$test = file_get_contents($weather,0);
-$xm = new SimplexmlElement($test);
-print_r($xm);
+$weather=file_get_contents ('http://api.openweathermap.org/data/2.5/weather?zip=83843,us&APPID=5888b8b93ed8d1906c9665e31bed55ff');
+//$test = file_get_contents($weather,0);
+//$xm = new SimplexmlElement($test);
+//print_r($xm);
+$obj = json_decode($weather);
+//print_r($weather)
+print $obj->{'name'};
+echo "<br/>";
+
+echo " Текующая температура ";
+print $obj->{'main'}->{'temp'};
+echo "<br/>";
+echo "Давление ";
+print $obj->main->pressure;
+echo "<br/>";
+echo "Cкорость ветра ";
+print $obj->wind->speed;
+echo "<br/>";
+echo "Влажность ";
+print $obj->main->humidity;
 ?>
